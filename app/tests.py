809a39c293dcc49d78b7a420a9765fa56e197331
mohammadtigons <122691572+mohammadtigons@users.noreply.github.com>
2023-05-30 14:51:28 +0330

    Update tests.py

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from django.test import LiveServerTestCase
+from selenium import webdriver
+import time
+# WEBDRIVER_PATH ='/home/blackhil/.cache/selenium/chromedriver/linux64/113.0.5672.63/webdriver'
+# SELENIUM_OPTIONS = [
+#     '--headless',  # Run in headless mode (without a visible browser window)
+#     '--no-sandbox',  # Add additional options as needed
+# ]
 
-# Create your tests here.
+class test(LiveServerTestCase):
+    class test(LiveServerTestCase):
+        driver_path = WEBDRIVER_PATH
+        options = webdriver.ChromeOptions()
+        for arg in SELENIUM_OPTIONS:
+                    options.add_argument(arg)
+        driver = webdriver.Chrome(executable_path=driver_path, options=options)
+        driver.get('https://www.google.com')
+        time.sleep(9)
+        driver.quit()
