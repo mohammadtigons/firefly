740ce428681af028040ca370228531d6210366f5
mohammadtigons <122691572+mohammadtigons@users.noreply.github.com>
2023-05-30 14:44:58 +0330

    Update settings.py

--- myfire/settings.py
+++ myfire/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6yvm8294cvisdhoi)vbr3jy@@$pclxu-pj^tjvbgvm5sj)tonm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["blackhill360.runflare.run","www.blackhill360.runflare.run"]
 
 
 # Application definition
