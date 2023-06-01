fa7172a356c0744719290e1591581447c0541f31
mohammadtigons <122691572+mohammadtigons@users.noreply.github.com>
2023-06-01 10:37:19 +0330

    Update tests.py

--- app/tests.py
+++ app/tests.py
@@ -20,7 +20,10 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 from selenium import webdriver
-driver = webdriver.Chrome()
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service 
+ser = Service(r"./driver/chromedriver.exe")
+driver = webdriver.Chrome(service=ser)
 driver.get("https://www.google.com")
 get_url = driver.current_url
 print("The current url is:"+str(get_url))
