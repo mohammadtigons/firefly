4a63e472bc37bab4c51edbf38640cc8841ab287e
mohammadtigons <122691572+mohammadtigons@users.noreply.github.com>
2023-06-01 10:48:04 +0330

    Update tests.py

--- app/tests.py
+++ app/tests.py
@@ -18,7 +18,7 @@ import time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+# driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 # from selenium import webdriver
 # from selenium.webdriver.chrome.service import Service 
 # ser = Service(r"./driver/chromedriver.exe")
